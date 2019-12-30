3665b2c58769c08a58f7b06840328f851a868f1e
miranda-ni <eralieva.miranda@gmail.com>
2019-12-30 18:24:22 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.lang.invoke.SwitchPoint;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] name2 = {"Alina", "Tima", "Amir"};
+        for (String hi : name2) {
+            System.out.println(hi);
+            switch (hi){
+                case "Alina":
+                    System.out.println("доброе утро");
+                    break;
+                case "Tima":
+                    System.out.println("доброго дня");
+                    break;
+                case "Amir":
+                    System.out.println("добрый вечер");
+                    break;
+
+            }
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
